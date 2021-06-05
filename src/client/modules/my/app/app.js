da1089127de21e0b9d4cf10504d6739bc6074628
ADMIN-PC\admin <abdul.basith@riskonnect.com>
2021-06-05 23:43:28 +0530

    updated changes

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -1,5 +1,28 @@
 import { LightningElement, track } from 'lwc';
 
 export default class App extends LightningElement {
+    
+    typeOptions = [
+        {
+            label: 'Expense',
+            value: 'Expense'
+        },
+        {
+            label: 'Income',
+            value: 'Income'
+        }
+    ]
+
+    categoryOptions = [
+        {
+            label: 'Fuel',
+            value: 'Fuel'
+        },
+        {
+            label: 'Grocery',
+            value: 'Grocery'
+        }
+    ]
+
     @track showSpinner = false;
 }
